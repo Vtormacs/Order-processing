2f0437d6550d1ffe939b47bdb00f3367cb74a7db
Vtormacs <macskids98play@hotmail.com>
2024-08-08 17:29:17 -0300

    Payment, one-to-one association

--- src/main/java/com/educaweb/projeto/Config/TesteConfig.java
+++ src/main/java/com/educaweb/projeto/Config/TesteConfig.java
@@ -71,6 +71,11 @@ public class TesteConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Paymant pay1 = new Paymant(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPaymant(pay1);
+
+        orderRepository.save(o1);
     }
 }
 
--- src/main/java/com/educaweb/projeto/Entity/Order.java
+++ src/main/java/com/educaweb/projeto/Entity/Order.java
@@ -33,6 +33,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Paymant paymant;
+
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
